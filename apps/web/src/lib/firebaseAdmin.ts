34cb7877bee34508058d24c6bbaa4378df26d952
ShreevathsaR <shreevathsar2002@gmail.com>
2025-07-22 12:44:07 +0530

    fix: next js deployment fixes

--- apps/web/src/lib/firebaseAdmin.ts
+++ apps/web/src/lib/firebaseAdmin.ts
@@ -3,11 +3,13 @@ import { getAuth } from 'firebase-admin/auth';
 
 const firebaseAdminConfig = {
   credential: cert({
-    projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-    clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
 };
+
+
 
 const app = getApps().length === 0 ? initializeApp(firebaseAdminConfig) : getApps()[0];
 export const adminAuth = getAuth(app);
--- apps/web/trpc/env.js
+++ apps/web/trpc/env.js
@@ -2,9 +2,9 @@ import dotenv from "dotenv";
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
 const __dirname = dirname(fileURLToPath(import.meta.url));
-dotenv.config({ path: join(__dirname, "../.env") });
+dotenv.config({ path: join(__dirname, "../.env.local") });
 export const env = {
     FIREBASE_PROJECT_ID: process.env.FIREBASE_PROJECT_ID,
     FIREBASE_CLIENT_EMAIL: process.env.FIREBASE_CLIENT_EMAIL,
-    FIREBASE_ADMIN_PRIVATE_KEY: process.env.FIREBASE_ADMIN_PRIVATE_KEY,
+    FIREBASE_ADMIN_PRIVATE_KEY: process.env.FIREBASE_PRIVATE_KEY,
 };
