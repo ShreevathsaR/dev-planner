798ca4f28c2b1020d7007a676732fb7ac6ee4872
ShreevathsaR <shreevathsar2002@gmail.com>
2025-07-23 16:02:07 +0530

    feat:deployment fixes

--- apps/web/trpc/server/redisClient.js
+++ apps/web/trpc/server/redisClient.js
@@ -1,3 +1,3 @@
 import Redis from "ioredis";
 import { env } from "../env";
-export const redis = new Redis(env.REDIS_URL || 'redis://localhost:6379');
+export const redis = new Redis(env.REDIS_URL || '');
